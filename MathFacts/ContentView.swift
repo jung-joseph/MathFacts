847274ad384f47ad26928937691ee8f9efbe3cdc
Joseph Jung <joseph.jung@icloud.com>
2019-09-13 12:41:16 -0600

    Clean up to get back to running code

--- MathFacts/ContentView.swift
+++ MathFacts/ContentView.swift
@@ -22,84 +22,98 @@ struct ContentView: View {
     
     var body: some View {
         
+        
         NavigationView{
-            VStack{
+            ZStack{
+                //Define the screen Color
+                Color.blue
+                    // Extend the screen to all edges
+                    .edgesIgnoringSafeArea(.all)
                 
-                HStack{
-                    Spacer()
-                    Text("Score: \(score) / \(numProblems)")
-                        .font(.largeTitle)
-                        .padding()
-                        .foregroundColor(Color.red)
-                }
-                HStack{
-                    Text("\(mathModel.number1)").font(.largeTitle)
-                    Text(" + ").font(.largeTitle)
-                    Text("\(mathModel.number2)").font(.largeTitle)
-                    Text(" = ").font(.largeTitle)
-                    //                    Text("\(mathModel.number3)").font(.largeTitle)
-                    Text("\(answerDisplay)").font(.largeTitle)
-                }.background(Color.white).padding().cornerRadius(10).shadow(radius: 10)
-                
-                
-                if rightWrong == "Right!" {
-                    Text("\(rightWrong)").foregroundColor(Color.green).font(.largeTitle)
-                } else {
-                    Text("\(rightWrong)").foregroundColor(Color.red).font(.largeTitle)
+                VStack{
                     
-                }
-                
-                TextField("Answer", text: $mathModel.answerText)
-                    .textFieldStyle(RoundedBorderTextFieldStyle()).padding().font(.custom("Arial", size: 40))
-                    .keyboardType(.numberPad)
-                
-                Button (action: {
-                    let answerTemp = Int(self.mathModel.answerText)
-                    if answerTemp == self.mathModel.number3 {
-                        print("Right")
-                        self.rightWrong = "Right!"
-                        self.answerDisplay = String(self.mathModel.number3)
-                        if self.newProblem {
-                            self.score += 1
-                        }
-                        
+                    HStack{
+                        Spacer()
+                        Text("Score: \(score) / \(numProblems)")
+                            .font(.largeTitle)
+                            .padding()
+                            .foregroundColor(Color.white)
+                    }
+                    
+                    HStack{
+                        Text("\(mathModel.number1)").font(.largeTitle)
+                        Text(" + ").font(.largeTitle)
+                        Text("\(mathModel.number2)").font(.largeTitle)
+                        Text(" = ").font(.largeTitle)
+                        //                    Text("\(mathModel.number3)").font(.largeTitle)
+                        Text("\(answerDisplay)").font(.largeTitle)
+                    }.background(Color.white).padding().cornerRadius(10).shadow(radius: 10)
+                    
+                    
+                    if rightWrong == "Right!" {
+                        Text("\(rightWrong)").foregroundColor(Color.green).font(.largeTitle)
                     } else {
-                        print("Sorry")
-                        self.rightWrong = "Sorry!"
-                        self.answerDisplay = String(self.mathModel.number3)
+                        Text("\(rightWrong)").foregroundColor(Color.red).font(.largeTitle)
                         
                     }
-                    self.newProblem = false
-                }){Text("Enter Answer")}
-                    .background(Color.red)
-                    .foregroundColor(Color.white)
-                    .cornerRadius(10)
-                    .shadow(radius: 10)
-                    .font(.largeTitle)
-                
-                Text("Double Tap For New Problem").font(.title).foregroundColor(Color.red)
-                
-                Spacer()
                     
-                    .navigationBarTitle("MathFacts!")
-                
-                
-            }
+                    TextField("Answer", text: $mathModel.answerText)
+                        .textFieldStyle(RoundedBorderTextFieldStyle()).padding().font(.custom("Arial", size: 40))
+                        .keyboardType(.numberPad)
+                    
+                    Button (action: {
+                        let answerTemp = Int(self.mathModel.answerText)
+                        if answerTemp == self.mathModel.number3 {
+                            print("Right")
+                            self.rightWrong = "Right!"
+                            self.answerDisplay = String(self.mathModel.number3)
+                            if self.newProblem {
+                                self.score += 1
+                            }
+                            
+                        } else {
+                            print("Sorry")
+                            self.rightWrong = "Sorry!"
+                            self.answerDisplay = String(self.mathModel.number3)
+                            
+                        }
+                        self.newProblem = false
+                    }){Text("Enter Answer")}
+                        .background(Color.red)
+                        .foregroundColor(Color.white)
+                        .cornerRadius(10)
+                        .shadow(radius: 10)
+                        .font(.largeTitle)
+                    
+                    Text("Double Tap For A New Problem")
+                        .font(.title)
+                        .foregroundColor(Color.white)
+                        .lineLimit(nil)
+                        .multilineTextAlignment(.center)
+                    
+                    Spacer()
+                    
+                    
+                    
+                }// VStack
+            } // ZStack
+                .navigationBarTitle("MathFacts!")
+            
+        }// NavigationView
+            .onTapGesture(count: 2){
+                print("Tapped!")
+                self.mathModel.addition()
+                self.answerDisplay = "??"
+                self.mathModel.answerText = ""
+                self.newProblem = true
+                self.numProblems += 1
+                self.rightWrong = ""
         }
-        .onTapGesture(count: 2){
-            print("Tapped!")
-            self.mathModel.addition()
-            self.answerDisplay = "??"
-            self.mathModel.answerText = ""
-            self.newProblem = true
-            self.numProblems += 1
-            self.rightWrong = ""
-        }
-
-        .background(Color.red)
-
+        
+        
     }
-}
+}// ContentView
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
